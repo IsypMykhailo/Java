696df6e51285973867de1f1fbc20e653a1b9894e
IsypMykhailo <amazonkanameforme@gmail.com>
2022-09-19 15:47:11 -0700

    thymeleaf start

--- firstSpring/src/main/java/com/itstep/firstspring/controllers/HomeController.java
+++ firstSpring/src/main/java/com/itstep/firstspring/controllers/HomeController.java
@@ -1,13 +1,34 @@
 package com.itstep.firstspring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
-    @GetMapping("/")
-    public String home(){
-        return "Hello";
+    private int count = 0;
+
+    @GetMapping("/err")
+    public String home1(){
+        return "home";
+    }
+
+    @GetMapping("/") // Маршрут, по которому будет отвечать данный метод котроллера
+    public String home(
+            Model model // Модель представления данных, которую мне нужно наполнять
+            // Это аналог ViewData ViewBug из ASP
+    ){
+        count++;
+
+        // Передача аналогична подходу в ASP
+        model.addAttribute("count", count);
+
+        int[] arr = {3,2,1};
+        model.addAttribute("arr",arr);
+
+        // return VIEW - имя представления!!!!
+        // return "<p>This page opens: "+count+"</p>";
+        return "home";
     }
 }
